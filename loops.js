953e6e5b6bfdbad4b7b87f7cbd73ff469b2a6235
Lori Kulikowski <soupfairy99@gmail.com>
2018-04-13 17:21:27 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -15,8 +15,9 @@ function forLoop(array)
 forLoop();
 
 function whileLoop(n) {
+  
   let countdown = n;
-  while (countdown > 0){console.log(--countdown)
+  while (countdown > 0) {console.log(--countdown)
 }
 return('done')
 }
